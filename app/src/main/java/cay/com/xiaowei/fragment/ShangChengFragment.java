0b62bc0952d50bf3879854f3723652e39d5a312b
Cay chen <276495166@qq.com>
2016-08-09 10:29:17 +0800

    更新了商城地址网络请求

--- app/src/main/java/cay/com/xiaowei/fragment/ShangChengFragment.java
+++ app/src/main/java/cay/com/xiaowei/fragment/ShangChengFragment.java
@@ -3,6 +3,8 @@ package cay.com.xiaowei.fragment;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,24 +17,45 @@ import com.youzan.sdk.web.event.ShareDataEvent;
 import com.youzan.sdk.web.plugin.YouzanBrowser;
 import com.youzan.sdk.web.plugin.YouzanChromeClient;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import cay.com.xiaowei.MyApplication;
 import cay.com.xiaowei.R;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by C on 2016/8/3.
  */
 public class ShangChengFragment extends Fragment {
+    private String SHANGPU_URL;
     public static YouzanBrowser mWebView;
-    private String URL = "https://wap.koudaitong.com/v2/feature/fgt82zc?&redirect_count=1967461";
+    private Handler urlHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String webUrl = (String) msg.obj;
+            mWebView.loadUrl(webUrl);
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shangcheng, null);
         mWebView = (YouzanBrowser) view.findViewById(R.id.youzan_webView);
-        mWebView.loadUrl(URL);
+        initUrls();
         return view;
     }
+
     /**
      * 根据需求订阅相应的桥接事件
      */
@@ -69,16 +92,41 @@ public class ShangChengFragment extends Fragment {
         });
     }
 
-private void initUrls(){
-    OkHttpClient urlHttpClient = new OkHttpClient();
-    Request urlRequest = new Request.Builder()
-         //   .url()
-            .build();
+    private void initUrls() {
+        try {
+            SHANGPU_URL = MyApplication.URL + "?key=" + MyApplication.API_KEY_XIAOWEI + "&info="
+                    + URLEncoder.encode("小微Splash_URL", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        OkHttpClient urlHttpClient = new OkHttpClient();
+        Request urlRequest = new Request.Builder()
+                .url(SHANGPU_URL)
+                .build();
+        Call urlCall = urlHttpClient.newCall(urlRequest);
+        urlCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String shangPuUrlJson = response.body().string();
+                try {
+                    JSONObject urlJsonObject = new JSONObject(shangPuUrlJson);
+                    final String shangPuUrl = urlJsonObject.getString("text");
+                    Message message = new Message();
+                    message.obj = shangPuUrl;
+                    urlHandler.sendMessage(message);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
-
-}
-
+    }
 
 
 }
